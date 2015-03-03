e30dd21616591baebb51bbcd48cca08db8c142cd
matt-matt <majhale4@gmail.com>
2015-03-02 19:55:52 -0800

    First

---
+++ MST.cpp
@@ -0,0 +1,163 @@
+#include "MST.h"
+#include <cmath>
+#include <stack>
+
+MST::MST(int** input, int size) {
+	adjacentMatrix = input;
+	key = new int[size];   
+    mstSet = new bool[size];  
+	parent = new int[size];
+    visited = new bool[size];
+
+	N = size;
+}
+
+MST::~MST() {
+
+}
+
+//use Prim's algorithm or Kruskal algorithm. Copied from 'http://www.geeksforgeeks.org/greedy-algorithms-set-5-prims-minimum-spanning-tree-mst-2/'
+void MST::makeTree() { 
+     // Initialize all keys as INFINITE
+     for (int i = 0; i < N; i++)
+        key[i] = INT_MAX, mstSet[i] = false;
+ 
+     // Always include first 1st vertex in MST.
+     key[0] = 0;     // Make key 0 so that this vertex is picked as first vertex
+     parent[0] = -1; // First node is always root of MST 
+ 
+     // The MST will have V vertices
+     for (int count = 0; count < N-1; count++)
+     {
+        // Pick thd minimum key vertex from the set of vertices
+        // not yet included in MST
+        int u = minKey(key, mstSet);
+ 
+        // Add the picked vertex to the MST Set
+        mstSet[u] = true;
+ 
+        // Update key value and parent index of the adjacent vertices of
+        // the picked vertex. Consider only those vertices which are not yet
+        // included in MST
+        for (int v = 0; v < N; v++)
+           // mstSet[v] is false for vertices not yet included in MST
+           // Update the key only if adjacentMatrix[u][v] is smaller than key[v]
+          if (adjacentMatrix[u][v] && mstSet[v] == false && adjacentMatrix[u][v] <  key[v])
+             parent[v]  = u, key[v] = adjacentMatrix[u][v];
+     }
+}
+
+// A utility function to find the vertex with minimum key value, from
+// the set of vertices not yet included in MST
+int MST::minKey(int key[], bool mstSet[])
+{
+   // Initialize min value
+   int min = INT_MAX, min_index;
+ 
+   for (int v = 0; v < N; v++)
+     if (mstSet[v] == false && key[v] < min)
+         min = key[v], min_index = v;
+ 
+   return min_index;
+}
+
+// A utility function to print the constructed MST stored in parent[]
+void MST::printMST() {
+	cout<<endl;
+	cout<<"Minimum spanning tree from the adjacency matrix"<<endl;
+	cout<<"Edge   Weight"<<endl;
+	for (int i = 1; i < N; i++) {
+		cout<<parent[i]<<" - "<<i<<"  "<<adjacentMatrix[i][parent[i]]<<endl;
+	}
+    float avg = calMean(MST_1);
+    cout<<"Mean edge weight of MST"<<endl;
+    cout<<avg<<endl;
+    cout<<"Standard Deviation of edge weight in MST"<<endl;
+    cout<<calStd(MST_1, avg)<<endl;
+}
+
+//calculate mean of all edges in the MST
+float MST::calMean(int option) {
+	float mean = 0.0;
+
+	if(option == MST_1) {
+
+        for (int i = 1; i < N; i++) {
+            mean += adjacentMatrix[i][parent[i]];
+        }
+
+        mean /= N;
+		
+	} else if(option == TSP2) {
+
+	} else if(option == TSP1_5) {
+
+	}
+
+	return mean;
+}
+
+//calculate standard deviation of all edges in the MST
+float MST::calStd(int option, int mean) {
+	float std = 0.0;
+    float variance = 0.0;
+
+	if(option == MST_1) {
+		
+        for (int i = 1; i < N; i++) {
+            variance += (float)pow(adjacentMatrix[i][parent[i]] - mean, 2);
+        }
+        variance /= N;
+        std = sqrt(variance);
+	} else if(option == TSP2) {
+
+	} else if(option == TSP1_5) {
+
+	}
+
+	return std;
+}
+
+void MST::makeTSP2() {
+	//make a Eulerian tour by DFS
+
+	//add shortcuts if a vertex has no detours.
+
+	//calculate heuristic TSP cost
+
+    dfs
+
+    
+    
+
+}
+
+void dfs(int u) {
+    visited[u] = true;
+    for (int v = 0; v < n; v++) {
+        if (!seen[v] && adjacentMatrix[u][v])   {
+            dfs(v);
+        }
+    }
+}
+
+void MST::makeTSP1_5() {
+	
+	//construct minimum-weight-matching for the given MST
+	minimumMatching();
+
+	//make all edges has even degree by combining mimimum-weight matching and MST
+	combine();
+
+	//calculate heuristic TSP cost 
+}
+
+void MST::minimumMatching() { //if you choose O(n^2)
+	//find minimum-weight matching for the MST. 
+	
+	//you should carefully choose a matching algorithm to optimize the TSP cost.
+}
+
+void MST::combine() {
+	//combine minimum-weight matching with the MST to get a multigraph which has vertices with even degree
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Euclidean-TSP
+Starter code for CSE 101 implementation project in Winter 2015.
+
+How to execute: 
+
+./make
+
+./TSP
+W = 16,811 H = 17,722 N = 9,596
